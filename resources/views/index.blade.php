ce117606e88dba6d1c50c5c091d7b57f07fb3ebf
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-10 16:38:43 +0100

    delete

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,16 +6,22 @@
 </h1>
 
 <div class="container d-flex align-items-center justify-content-center flex-wrap my-2">
-    
+
     @foreach ($comics as $item)
     <div class="card mx-2" style="width: 18rem;">
         <img src="{{$item->thumb}}" class="card-img-top" alt="{{$item["title"]}}">
         <div class="card-body">
             <h2> {{$item["title"]}}</h2>
             <p class="card-text">{{$item["sale_date"]}}</p>
-           <a href="{{route("comics.show", $item->id)}}"><button type="button" class="btn btn-success">SEE MORE</button></a>
-            <a href="{{route("comics.edit", $item->id)}}"><button type="button" class="btn btn-primary">CUSTOM</button></a>
-            <button type="button" class="btn btn-danger mx-2">DELETE</button>
+            <a href="{{route("comics.show", $item->id)}}"><button type="button" class="btn btn-success">SEE
+                    MORE</button></a>
+            <a href="{{route("comics.edit", $item->id)}}"><button type="button"
+                    class="btn btn-primary">CUSTOM</button></a>
+            <form action="{{route('comics.destroy', $item->id)}}" method="POST">
+                @csrf
+                @method("DELETE")
+                <button type="submit" class="btn btn-danger mx-2">DELETE</button>
+            </form>
         </div>
     </div>
     @endforeach
